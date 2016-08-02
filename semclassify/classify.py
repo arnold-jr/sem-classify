dade848da6a22f8da9be704790d07b3a0d0c01f3
arnold-jr <j.arnold.111@gmail.com>
2016-08-02 13:07:37 -0400

    testing online classifiers.

--- semclassify/classify.py
+++ semclassify/classify.py
@@ -2,15 +2,19 @@ import pandas as pd
 import numpy as np
 from semclassify.helpers import stopwatch
 from semclassify.transformers import ColumnSelectorTransformer
-from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.pipeline import Pipeline, FeatureUnion, make_pipeline
 from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import (SGDClassifier,
+                                  Perceptron,
+                                  LogisticRegression,
+                                  PassiveAggressiveClassifier)
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier
 from sklearn.naive_bayes import GaussianNB
-from sklearn.cross_validation import StratifiedShuffleSplit
+from sklearn.cross_validation import (StratifiedShuffleSplit, cross_val_score,
+                                      train_test_split)
 from sklearn.grid_search import GridSearchCV
 import dill as pickle
 
@@ -28,12 +32,6 @@ class SuperModel():
       raise
 
     self.modelChoices = dict(
-      logit=dict(model=LogisticRegression(
-        class_weight='balanced',
-        penalty='l1',
-      ),
-        param_grid={'C': np.logspace(-2, 2, 3)}
-      ),
       lda=dict(model=LinearDiscriminantAnalysis(
         n_components=None,
         priors=None,
@@ -106,53 +104,75 @@ class SuperModel():
       ),
         param_grid={"class_weight": ['balanced',None,'balanced_subsample']}
       ),
-      gnb=dict(model=GaussianNB(),
-               param_grid=dict()
-               ),
+      gnb=dict(model=GaussianNB()),
+      sgd=dict(model=SGDClassifier()),
+      asgd=dict(model=SGDClassifier(average=True)),
+      perceptron=dict(model=Perceptron()),
+      pag1=dict(model=PassiveAggressiveClassifier(loss='hinge',
+                                                  C=1.0)
+                ),
+      pag2=dict(model=PassiveAggressiveClassifier(loss='squared_hinge',
+                                                          C=1.0)
+                ),
+      logit=dict(model=SGDClassifier(loss='log'))
     )
 
-  def train_model(self, chunk, model_name, model_choice, all_classes=None):
+  def train_model(self, chunk, model_name, model_values, all_classes=None):
 
-    model_pipe = Pipeline([
-      ('features', FeatureUnion(
-        [('numeric', ColumnSelectorTransformer(self.feat_cols)),]
-      )),
+    feature_steps = [
+      ('features', FeatureUnion([
+        ('numeric', ColumnSelectorTransformer(self.feat_cols)),
+      ])
+      ),
       ('scaler', StandardScaler()),
-      ('model', model_choice['model']),
-    ])
+    ]
+
+    feature_pipe = Pipeline(feature_steps)
+
+    model_pipe = Pipeline(feature_steps +
+                               [('model', model_values['model'])]
+                          )
 
     y = chunk.loc[:, self.label_col].as_matrix().ravel()
 
     cv = StratifiedShuffleSplit(y, n_iter=5, test_size=0.3)
-    clf_CV = GridSearchCV(model_pipe,
-                          param_grid={'model__'+k:v
-                                      for k,v in
-                                      model_choice['param_grid'].items()},
-                          cv=cv,
-                          n_jobs=1)
-    if True:
-      clf_CV.fit(chunk, y)
+
+    if False:
+      param_grid = model_values.get('param_grid',None)
+      if param_grid is not None:
+        clf_CV = GridSearchCV(model_pipe,
+                              param_grid={'model__'+k:v
+                                          for k,v in
+                                          param_grid.items()},
+                              cv=cv,
+                              n_jobs=1)
+        clf_CV.fit(chunk, y)
+        print(model_values['model'])
+        print('Model: %s\t Score: %f\t' % (model_name, clf_CV.best_score_))
+        print('Parameters: ', clf_CV.best_params_)
+      else:
+        scores = cross_val_score(model_pipe, chunk, y, cv=cv, n_jobs=-1)
+        print('Model: %s\t Score: %f\t' % (model_name, scores.mean()))
     else:
-      clf_CV.partial_fit(chunk, y, all_classes)
-
-    print(model_choice['model'])
-    print('Model: %s\t Score: %f\t' % (model_name, clf_CV.best_score_))
-    print('Parameters: ', clf_CV.best_params_)
-
-    '''
-    with open(self.get_pickled_name(model_name),
-              'wb') as pckl_output:
-      pickle.dump(clf_CV.best_estimator_, pckl_output)
-    '''
+      X_tform = feature_pipe.fit_transform(chunk)
+      X_train, X_test, y_train, y_test = train_test_split(X_tform, y,
+                                                          test_size=0.3,
+                                                          random_state=42)
+      clf = model_values['model']
+      clf.partial_fit(X_train, y_train, classes=all_classes)
+      scores = clf.score(X_test, y_test)
+      print('Model: %s\t Score: %f\t' % (model_name, scores.mean()))
+      return clf
 
 
-  def train_all_models(self, model_names=('dtc')):
+  def train_all_models(self, model_names=('gnb')):
     """ Trains all the classifiers specified by their model name.
 
     :param model_names: list of model code names as strings
     :return None
     """
-    where = 'site="soi_001"'
+    # where = 'site="soi_001"'
+    where = None
     df_labels = pd.read_hdf(self.store_path,
                             'df',
                             where=where,
@@ -160,9 +180,9 @@ class SuperModel():
                             )
     all_classes = np.unique(df_labels)
     n_rows = len(df_labels.index)
-    chunksize = n_rows
+    chunksize = n_rows // 10
 
-    for chunk_iter in pd.read_hdf(self.store_path,
+    for chunk in pd.read_hdf(self.store_path,
                              'df',
                              chunksize=chunksize,
                              where=where,
@@ -171,13 +191,34 @@ class SuperModel():
         if modelName not in model_names:
           continue
         with stopwatch('training model %s' % modelName):
-          chunk = chunk_iter
-          self.train_model(chunk, modelName, modelValues, all_classes)
-          print(chunk)
+          modelValues.update(
+            dict(model=self.train_model(chunk,
+                                        modelName,
+                                        modelValues,
+                                        all_classes)
+                 )
+          )
 
 
   def get_pickled_name(self, model_name):
-    return self.fpath.replace(".csv","_" + model_name + ".dpkl")
+    """ Returns a transformation of a name for a pickled model.
+
+    :param model_name: unique id of a trained model
+    :return path to the model
+    """
+    return self.store_path.replace(".h5","_" + model_name + ".dpkl")
+
+
+  def pickle_models(self, model_names):
+    """ Pickles selected models using the dill package.
+
+    :return None
+    """
+    for model_name in model_names:
+      with open(self.get_pickled_name(model_name),
+                'wb') as pckl_output:
+        pickle.dump(self.modelChoices[model_name]['model'], pckl_output)
+
 
 
   def get_trained_model(self, model_name):
@@ -199,6 +240,6 @@ class SuperModel():
 if __name__ == "__main__":
   sm = SuperModel(
     "/Users/joshuaarnold/Documents/MyApps/sem-classify/output/store.h5",
-    ['BSE'],
+    ['Al','Si','Ca','S','BSE'],
     ['BFS'])
   sm.train_all_models()
